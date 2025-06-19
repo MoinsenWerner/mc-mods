f36fbbcb8588b6b2526cfc7f466e046d2a76d544
MoinsenWerner <felix.h.kaiser@outlook.de>
2025-06-19 20:30:47 +0200

    updated all

--- duel-mod/src/main/java/com/example/duel/DuelMod.java
+++ duel-mod/src/main/java/com/example/duel/DuelMod.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import com.example.duel.command.DuelCommands;
 
 @Mod(DuelMod.MODID)
 public class DuelMod {
